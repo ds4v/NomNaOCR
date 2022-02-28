3d22490f953fbba234e695db2c43044ab67d62be
Quan Dang <18520339@gm.uit.edu.vn>
2022-02-28 19:12:50 +0700

    Fix write sentence when text contains double newline

--- Data collection/json2txt.py
+++ Data collection/json2txt.py
@@ -10,21 +10,22 @@ ap.add_argument(
     type = FileType('r', encoding='utf-8')
 )
 args = vars(ap.parse_args())
+# Example: python json2txt.py --infile "Tale of Kieu version 1866/automa.json"
 
 data = json.load(args['infile'])
 base_url = 'http://www.nomfoundation.org'
 out_dir = os.path.dirname(args['infile'].name)
 
 url_path = os.path.join(out_dir, 'url.txt')
-modern_path = os.path.join(out_dir, 'modern.txt')
 nom_path = os.path.join(out_dir, 'nom.txt')
+modern_path = os.path.join(out_dir, 'modern.txt')
 
 with open(url_path, 'w', encoding='utf-8') as url_file:
-    with open(modern_path, 'w', encoding='utf-8') as modern_file:
-        with open(nom_path, 'w', encoding='utf-8') as nom_file:
+    with open(nom_path, 'w', encoding='utf-8') as nom_file:
+        with open(modern_path, 'w', encoding='utf-8') as modern_file:
             for page in data:
                 url_file.write(base_url + page['url'] + '\n')
-                text = page['text'].split('\n')
+                text = page['text'].replace('\n\n', '\n').split('\n')
 
                 for idx, sentence in enumerate(text):
                     if sentence == '': continue
