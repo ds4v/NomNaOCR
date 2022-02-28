71f22c460b8a3a424e5ea7083990cf932016e428
Quan Dang <18520339@gm.uit.edu.vn>
2022-02-28 20:01:50 +0700

    Extract nom vocabs in automa JSON result

--- Data collection/json2txt.py
+++ Data collection/json2txt.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser, FileType
 import json
 import os
+import sys
 
 ap = ArgumentParser()
 ap.add_argument(
@@ -12,23 +13,31 @@ ap.add_argument(
 args = vars(ap.parse_args())
 # Example: python json2txt.py --infile "Tale of Kieu version 1866/automa.json"
 
+out_dir = os.path.dirname(args['infile'].name)
 data = json.load(args['infile'])
 base_url = 'http://www.nomfoundation.org'
-out_dir = os.path.dirname(args['infile'].name)
+vocabs = []
 
 url_path = os.path.join(out_dir, 'url.txt')
 nom_path = os.path.join(out_dir, 'nom.txt')
 modern_path = os.path.join(out_dir, 'modern.txt')
+vocabs_path = os.path.join(out_dir, 'vocabs.txt')
 
-with open(url_path, 'w', encoding='utf-8') as url_file:
-    with open(nom_path, 'w', encoding='utf-8') as nom_file:
-        with open(modern_path, 'w', encoding='utf-8') as modern_file:
-            for page in data:
-                url_file.write(base_url + page['url'] + '\n')
-                text = page['text'].replace('\n\n', '\n').split('\n')
+with open(url_path, 'w', encoding='utf-8') as url_file, \
+     open(nom_path, 'w', encoding='utf-8') as nom_file, \
+     open(modern_path, 'w', encoding='utf-8') as modern_file, \
+     open(vocabs_path, 'w', encoding='utf-8') as vocabs_file:
 
-                for idx, sentence in enumerate(text):
-                    if sentence == '': continue
-                    if idx % 2 == 0: nom_file.write(sentence + '\n')
-                    else: modern_file.write(sentence + '\n')
+    for page in data:
+        url_file.write(base_url + page['url'] + '\n')
+        text = page['text'].replace('\n\n', '\n').split('\n')
 
+        for idx, sentence in enumerate(text):
+            if sentence == '': continue
+            if idx % 2 == 0: 
+                nom_file.write(sentence + '\n')
+                vocabs.extend(list(sentence))
+            else: modern_file.write(sentence + '\n')
+
+    vocabs = set(vocabs) # Remove duplicates in vocabs
+    vocabs_file.write('\n'.join(vocabs))
