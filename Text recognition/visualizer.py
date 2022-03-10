ab42a786ab8e6751502e785d1790be125dc9cd40
Quan Dang <18520339@gm.uit.edu.vn>
2022-03-10 20:13:21 +0700

    Shorten _draw_predicted_text if conditions

--- Text recognition/visualizer.py
+++ Text recognition/visualizer.py
@@ -5,29 +5,25 @@ from matplotlib.font_manager import FontProperties
 
 def _draw_predicted_text(label, pred_label, fontdict, text_x):
     label = label.replace('[UNK]', ' ?')
-    label_length = len(label)
+    label_length, pred_length = len(label), len(pred_label)
 
     if pred_label != label:
         start, end = 0, 0
         while start <= end < label_length:
             text_y = end * label_length * 3
-            if label[start:end+1] in pred_label:
+            if label[start:end + 1] in pred_label:
                 fontdict['color'] = 'dodgerblue'
                 plt.text(text_x, text_y, label[end], fontdict=fontdict)
             else:
-                if end + 1 < label_length and pred_label[end] == label[end + 1]:
+                if (end >= pred_length or end + 1 < label_length) and pred_label[end] == label[end + 1]:
                     fontdict['color'] = 'gray'
                     plt.text(text_x, text_y, label[end], fontdict=fontdict)
                 else:
                     actual_char = '[UNK]' if label[end] == '?' else label[end]
-                    if end < len(pred_label):
-                        fontdict['color'] = 'red'
-                        plt.text(text_x, text_y, pred_label[end], fontdict=fontdict)
-                        fontdict['color'] = 'black'
-                        plt.text(text_x + 20, text_y, actual_char, fontdict=fontdict)
-                    else: 
-                        fontdict['color'] = 'gray'
-                        plt.text(text_x, text_y, label[end], fontdict=fontdict)
+                    fontdict['color'] = 'red'
+                    plt.text(text_x, text_y, pred_label[end], fontdict=fontdict)
+                    fontdict['color'] = 'black'
+                    plt.text(text_x + 20, text_y, actual_char, fontdict=fontdict)
                 start = end + 1
             end += 1
     else:
@@ -49,7 +45,6 @@ def visualize_images_labels(
     num_of_labels = len(labels)
     assert len(img_paths) == num_of_labels, 'img_paths and labels must have same number of items'
     assert nrows * ncols <= num_of_labels, f'nrows * ncols must be <= {num_of_labels}'
-
     fontdict = {
         'fontproperties': FontProperties(fname=font_path),
         'fontsize': 18,
@@ -57,14 +52,6 @@ def visualize_images_labels(
         'verticalalignment': 'top',
         'horizontalalignment': 'left'
     }
-
-    mpatches_handles = [
-        Patch(color='green', label='Full match'),
-        Patch(color='dodgerblue', label='Character match'),
-        Patch(color='red', label='Wrong prediction'),
-        Patch(color='black', label='Actual character'),
-        Patch(color='gray', label='Missing position'),
-    ]
 
     plt.figure(figsize=figsize)
     for i in range(min(nrows * ncols, num_of_labels)):
@@ -79,7 +66,13 @@ def visualize_images_labels(
 
     if show_legend and pred_labels is not None:
         plt.subplots_adjust(left=0, right=0.75)
-        plt.legend(handles=mpatches_handles, loc=(2.7, 1.75))
+        plt.legend(handles=[
+            Patch(color='green', label='Full match'),
+            Patch(color='dodgerblue', label='Character match'),
+            Patch(color='red', label='Wrong prediction'),
+            Patch(color='black', label='Actual character'),
+            Patch(color='gray', label='Missing position'),
+        ], loc=(2.7, 1.75))
 
         annotate_text = [f'{idx + 1:02d}. {text}' for idx, text in enumerate(pred_labels)]
         plt.annotate(
