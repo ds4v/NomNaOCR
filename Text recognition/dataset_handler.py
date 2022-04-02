b979663a6bc19957992b8fb5f98d68dd663865d4
Quan Dang <18520339@gm.uit.edu.vn>
2022-04-03 05:32:18 +0700

    Update dataset_handler.py
    1. Fix wrong vocab frequencies when removing rare characters
    2. Use `prepare_tf_dataset` as dataset_handler module
    3. Add tokens2texts function for converting batch of tokens to string

--- Text recognition/dataset_handler.py
+++ Text recognition/dataset_handler.py
@@ -3,7 +3,7 @@ import re
 import numpy as np
 import tensorflow as tf
 from string import punctuation
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 
 def is_clean_text(text):
@@ -25,36 +25,39 @@ def create_dataset(dataset_dir, labels_path, min_length=4):
             if os.path.getsize(img_path) and len(text) >= min_length and is_clean_text(text):
                 img_paths.append(img_path)
                 labels.append(text)
-                for char in text: vocabs[char] += 1
             
-    vocabs = dict(sorted(
-        vocabs.items(),
-        key = lambda item: item[1],
-        reverse = True
-    ))
-    return np.array(img_paths), np.array(labels), vocabs
+    sorted_vocabs = dict(Counter(''.join(labels)).most_common())
+    return np.array(img_paths), np.array(labels), sorted_vocabs
 
 
-def remove_rare_chars(img_paths, labels, vocabs, threshold=3):
-    if threshold < 2: return img_paths, labels, vocabs
-    rare_chars, new_vocabs = [], vocabs.copy()
-    
-    for char, freq in vocabs.items():
-        if freq < threshold:
-            rare_chars.append(char)
-            del new_vocabs[char]
+def remove_rare_chars(img_paths, labels, sorted_vocabs, threshold=1):
+    if threshold < 2: return img_paths, labels, sorted_vocabs
+    rare_chars, idxs_to_remove = [], []
+    is_satisfy_threshold = True
 
-    idxs_to_remove = []
+    # Vocabs need to be sorted (for faster checking)
+    for char, freq in reversed(sorted_vocabs.items()):
+        if freq < threshold: rare_chars.append(char)
+        else: break
+
     for idx, label in enumerate(labels):
         if any((char in label) for char in rare_chars):
             idxs_to_remove.append(idx)
 
+    # Remove sentences containing rare characters and recalculate the vocab frequencies
     idxs_to_remove = np.array(idxs_to_remove)
     img_paths = np.delete(img_paths, idxs_to_remove)
     labels = np.delete(labels, idxs_to_remove)
+    new_vocabs = dict(Counter(''.join(labels)).most_common())
+
+    # Check if there are still rare characters after removing sentences
+    smallest_freq = threshold + 1 # If vocabs is empty, the smallest frequency always > threshold
+    if len(new_vocabs) >= 1: smallest_freq = list(new_vocabs.values())[-1] 
+    no_more_rares = False if smallest_freq < threshold else True
 
     assert len(img_paths) == len(labels), 'img_paths and labels not same size'
-    return img_paths, labels, new_vocabs
+    if no_more_rares: return img_paths, labels, new_vocabs
+    return remove_rare_chars(img_paths, labels, new_vocabs, threshold)
 
 
 def distortion_free_resize(image, img_size):
@@ -85,9 +88,57 @@ def distortion_free_resize(image, img_size):
     ], constant_values=255)  # Pad with white color
 
 
-def process_one_image(image_path, img_size):
-    image = tf.io.read_file(image_path)
+def process_image(img_path, img_size):
+    image = tf.io.read_file(img_path)
     image = tf.image.decode_jpeg(image, 1)
     image = distortion_free_resize(image, img_size)
     image = tf.cast(image, tf.float32) / 255.0
     return image
+
+
+def process_label(label, max_length, char2num_func, extra_tokens={}):
+    label = char2num_func(tf.strings.unicode_split(label, input_encoding='UTF-8'))
+    if {'START', 'END'} <= extra_tokens.keys(): 
+        start_token = extra_tokens['START']
+        end_token = extra_tokens['END']
+        label = tf.concat([[start_token], label, [end_token]], 0)
+
+    label_length = tf.shape(label, tf.int64)[0]
+    label = tf.pad(
+        label, 
+        paddings = [[0, max_length - label_length]], 
+        constant_values = extra_tokens['PAD']
+    )
+    return label
+
+
+def prepare_tf_dataset(
+    elements, img_size, max_length, batch_size, char2num_func,
+    extra_tokens = {'START': None, 'END': None, 'PAD': None},
+    drop_remainder = False
+):
+    dataset = tf.data.Dataset.from_tensor_slices(elements).map(
+        lambda img_path, label: (
+            process_image(img_path, img_size),
+            process_label(label, max_length, char2num_func, extra_tokens)
+        ),
+        num_parallel_calls = tf.data.AUTOTUNE
+    )
+    return dataset.batch(batch_size, drop_remainder).cache().prefetch(tf.data.AUTOTUNE)
+
+
+def tokens2texts(batch_tokens, padding_token, num2char_func, removed_strs=[]):
+    batch_texts = []
+    for tokens in batch_tokens:
+        # Gather indices where label != padding_token.
+        not_padding = tf.math.not_equal(tokens, padding_token)
+        indices = tf.gather(tokens, tf.where(not_padding))
+
+        # Convert to string
+        text = tf.strings.reduce_join(num2char_func(indices)) 
+        text = text.numpy().decode('utf-8')
+
+        for string in removed_strs: 
+            text = text.replace(string, '')
+        batch_texts.append(text)
+    return batch_texts
