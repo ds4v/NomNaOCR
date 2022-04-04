2e0dff5178115c874a6414ad909e6dfd68842575
Quan Dang <18520339@gm.uit.edu.vn>
2022-04-04 20:35:33 +0700

    Reuse the old EarlyStopping without edist monitor

--- Text recognition/callbacks.py
+++ Text recognition/callbacks.py
@@ -2,11 +2,9 @@ import tensorflow as tf
 
 
 class EarlyStoppingWithStuck(tf.keras.callbacks.Callback):
-    def __init__(self, edist_metric_name='edist', patience=0, min_delta=0, stuck_str=None):
+    def __init__(self, patience=0, stuck_str=None):
         super(EarlyStoppingWithStuck, self).__init__()
-        self.edist_metric_name = edist_metric_name
         self.patience = patience
-        self.min_delta = min_delta
         self.stuck_str = stuck_str
         self.best_weights = None
 
@@ -14,22 +12,13 @@ class EarlyStoppingWithStuck(tf.keras.callbacks.Callback):
         self.wait = 0 # Number of epoch it has waited when loss is no longer minimum
         self.stopped_epoch = 0 # The epoch the training stops at
         self.best_loss = float('inf') # Initialize the best loss as infinity
-        self.best_edist = float('inf') # Initialize the best mean edit distance as infinity
         self.best_epoch = 0
 
     def on_epoch_end(self, epoch, logs=None):
-        loss = logs.get('loss')
-        edist = logs.get(self.edist_metric_name)
-
-        val_loss = logs.get('val_loss')
-        val_edist = logs.get(f'val_{self.edist_metric_name}')
-
-        if (self.best_loss - val_loss > self.min_delta or 
-            self.best_edist - val_edist > self.min_delta) and \
-            not eval(self.stuck_str):
+        loss, val_loss = logs.get('loss'), logs.get('val_loss')
+        if tf.less(val_loss, self.best_loss) and not eval(self.stuck_str):
             self.wait = 0
             self.best_loss = val_loss
-            self.best_edist = val_edist
             self.best_epoch = epoch
             self.best_weights = self.model.get_weights()
         else:
@@ -38,8 +27,8 @@ class EarlyStoppingWithStuck(tf.keras.callbacks.Callback):
                 self.stopped_epoch = epoch
                 self.model.stop_training = True
                 self.model.set_weights(self.best_weights)
-                
+
     def on_train_end(self, logs=None):
         if self.stopped_epoch > 0:
-            print(f'Early stopping and restored weights from epoch {self.best_epoch + 1}' 
-                  f' - val_loss: {self.best_loss:.4f} - val_edist: {self.best_edist:.4f}\n')
+            print(f'Early stopping and restored the model weights from the end of ' 
+                  f'epoch {self.best_epoch + 1} - val_loss: {self.best_loss}\n')
--- Text recognition/kfold.py
+++ Text recognition/kfold.py
@@ -12,17 +12,17 @@ def kfold_decorator(n_splits, random_state=None, is_subclassed_model=False):
 
     def decorator(func):
         def wrapper(model, img_paths, labels, *args, **kwargs):
-            for fold_idx, (train_idx, valid_idx) in enumerate(kf.split(img_paths, labels)):
+            for fold_idx, (train_idxs, valid_idxs) in enumerate(kf.split(img_paths, labels)):
                 if not is_subclassed_model: reset_model = clone_model(model) 
                 else: reset_model = model.__class__.from_config(model.get_config())
 
                 reset_model._name = f'Model_{fold_idx + 1:02d}'
                 print(f'============== Fold {fold_idx + 1:02d} training ==============')
                 valid_tf_dataset, best_epoch, history, reset_model = func(
-                    reset_model, img_paths, labels, train_idx, valid_idx
+                    reset_model, img_paths, labels, train_idxs, valid_idxs
                 )
 
-                valid_datasets.append((valid_tf_dataset, valid_idx))
+                valid_datasets.append((valid_tf_dataset, valid_idxs))
                 best_epochs.append(best_epoch)
                 histories.append(history)
                 models.append(reset_model)
@@ -35,13 +35,13 @@ def get_best_fold(valid_datasets, best_epochs, histories, models, verbose=1):
     best_loss = float('inf')
     for fold_idx, model in enumerate(models):
         steps = None
-        valid_tf_dataset, valid_idx = valid_datasets[fold_idx]
+        valid_tf_dataset, valid_idxs = valid_datasets[fold_idx]
         dataset_size = tf.data.experimental.cardinality(valid_tf_dataset).numpy()
 
         if dataset_size == tf.data.experimental.INFINITE_CARDINALITY:
             for batch in valid_tf_dataset.take(1): 
                 batch_size = batch['image'].shape[0]
-            steps = len(valid_idx) // batch_size
+            steps = len(valid_idxs) // batch_size
 
         final_val_loss = model.evaluate(valid_tf_dataset, steps=steps, verbose=verbose)[1]
         if final_val_loss < best_loss:
