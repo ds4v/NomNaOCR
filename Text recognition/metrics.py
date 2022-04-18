e1d71967b77a7bee634a2ce11afaae5b065c3aaa
Quan Dang <18520339@gm.uit.edu.vn>
2022-04-18 12:31:39 +0700

    Add cer and accuracy metrics

--- Text recognition/metrics.py
+++ Text recognition/metrics.py
@@ -1,57 +1,147 @@
+# https://github.com/FLming/CRNN.tf2/blob/master/crnn/metrics.py
 import tensorflow as tf
-from utils import ctc_decode
+from utils import ctc_decode, tokens2sparse, sparse2dense
 
 
-class MeanEditDistance(tf.keras.metrics.Metric):
+class SequenceAccuracy(tf.keras.metrics.Metric):
     def __init__(
         self, 
         padding_token = 0,
-        max_length = 0, # Required if use_ctc_decode == True
         use_ctc_decode = False, # Need to decode predictions if CTC loss used,
-        name = 'edist', 
+        name = 'seq_acc', 
         **kwargs
     ):
-        super(MeanEditDistance, self).__init__(name=name, **kwargs)
-        self.epoch_edit_distance = self.add_weight(name='med', initializer='zeros')
+        super(SequenceAccuracy, self).__init__(name=name, **kwargs)
         self.padding_token = padding_token
         self.use_ctc_decode = use_ctc_decode
-        self.max_length = max_length
-
-    # https://github.com/solivr/tf-crnn/blob/master/tf_crnn/model.py#L157
-    def _calculate_batch_edist(self, y_true, y_pred):
-        # Get a single batch and convert its labels to sparse tensors.
-        idxs = tf.where(y_true != self.padding_token)
-        sparse_true = tf.SparseTensor(
-            tf.cast(idxs, tf.int64),
-            tf.gather_nd(y_true, idxs),
-            tf.cast(tf.shape(y_true), tf.int64)
-        )
-
-        idxs = tf.where(tf.logical_and(
-            y_true != self.padding_token, 
-            y_true != -1 # For blank labels if use_ctc_decode 
-        ))
-        sparse_pred = tf.SparseTensor(
-            tf.cast(idxs, tf.int64),
-            tf.gather_nd(y_pred, idxs),
-            tf.cast(tf.shape(y_pred), tf.int64)
-        )
-
-        # Compute individual edit distances and average them out.
-        # https://stackoverflow.com/questions/51612489
-        edit_distances = tf.edit_distance(sparse_pred, sparse_true, normalize=False)
-        return tf.reduce_mean(edit_distances)
-
+        self.count = self.add_weight(name='count', initializer='zeros')
+        self.total = self.add_weight(name='total', initializer='zeros')
+                
     def update_state(self, y_true, y_pred, **kwargs):
-        if self.use_ctc_decode: y_pred = ctc_decode(y_pred, self.max_length)
-        self.epoch_edit_distance.assign(tf.reduce_mean([
-            self.epoch_edit_distance, 
-            self._calculate_batch_edist(y_true, y_pred)
-        ]))
-    
+        batch_size, max_length = tf.shape(y_true)[0], tf.shape(y_true)[1]
+        if self.use_ctc_decode: y_pred = ctc_decode(y_pred, max_length)
+
+        # Get a single batch and convert its labels to sparse tensors.
+        sparse_true = tokens2sparse(y_true, self.padding_token)
+        sparse_pred = tokens2sparse(y_pred, self.padding_token)
+
+        y_true = sparse2dense(sparse_true, [batch_size, max_length])
+        y_pred = sparse2dense(sparse_pred, [batch_size, max_length])
+
+        num_errors = tf.reduce_any(y_true != y_pred, axis=1)
+        num_errors = tf.reduce_sum(tf.cast(num_errors, tf.float32))
+        total = tf.cast(batch_size, tf.float32)
+
+        self.count.assign_add(total - num_errors)
+        self.total.assign_add(total)
+
     def result(self):
-        return self.epoch_edit_distance
+        return tf.math.divide_no_nan(self.count, self.total)
 
     def reset_state(self):
         # The state of the metric will be reset at the start of each epoch.
-        self.epoch_edit_distance.assign(0.0)
+        self.count.assign(0)
+        self.total.assign(0)
+
+
+class CharacterAccuracy(tf.keras.metrics.Metric):
+    def __init__(
+        self, 
+        padding_token = 0,
+        use_ctc_decode = False, # Need to decode predictions if CTC loss used,
+        name = 'char_acc', 
+        **kwargs
+    ):
+        super(CharacterAccuracy, self).__init__(name=name, **kwargs)
+        self.padding_token = padding_token
+        self.use_ctc_decode = use_ctc_decode
+        self.count = self.add_weight(name='count', initializer='zeros')
+        self.total = self.add_weight(name='total', initializer='zeros')
+                
+    def update_state(self, y_true, y_pred, **kwargs):
+        batch_size, max_length = tf.shape(y_true)[0], tf.shape(y_true)[1]
+        if self.use_ctc_decode: y_pred = ctc_decode(y_pred, max_length)
+
+        mask = tf.logical_and(
+            y_true != self.padding_token, 
+            y_true != -1 # For blank labels if use_ctc_decode 
+        )
+        num_errors = tf.logical_and(y_true != y_pred, mask)
+        num_errors = tf.reduce_sum(tf.cast(num_errors, tf.float32))
+        total = tf.reduce_sum(tf.cast(mask, tf.float32))
+
+        self.count.assign_add(total - num_errors)
+        self.total.assign_add(total)
+
+    def result(self):
+        return tf.math.divide_no_nan(self.count, self.total)
+
+    def reset_state(self):
+        # The state of the metric will be reset at the start of each epoch.
+        self.count.assign(0)
+        self.total.assign(0)
+
+
+class LevenshteinDistance(tf.keras.metrics.Metric):
+    def __init__(
+        self, 
+        padding_token = 0,
+        use_ctc_decode = False, # Need to decode predictions if CTC loss used,
+        normalize = False, # If True, this becomes Character Error Rate: CER = (S + D + I) / N
+        name = 'levenshtein_distance', 
+        **kwargs
+    ):
+        super(LevenshteinDistance, self).__init__(name=name, **kwargs)
+        self.padding_token = padding_token
+        self.use_ctc_decode = use_ctc_decode
+        self.normalize = normalize
+        self.sum_distance = self.add_weight(name='sum_distance', initializer='zeros')
+        self.total = self.add_weight(name='total', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, **kwargs):
+        batch_size, max_length = tf.shape(y_true)[0], tf.shape(y_true)[1]
+        if self.use_ctc_decode: y_pred = ctc_decode(y_pred, max_length)
+
+        # Get a single batch and convert its labels to sparse tensors.
+        sparse_true = tokens2sparse(y_true, self.padding_token)
+        sparse_pred = tokens2sparse(y_pred, self.padding_token)
+
+        # Explain tf.edit_distance: https://stackoverflow.com/questions/51612489
+        edit_distances = tf.edit_distance(sparse_pred, sparse_true, normalize=self.normalize)
+        self.sum_distance.assign_add(tf.reduce_sum(edit_distances))
+        self.total.assign_add(tf.cast(batch_size, tf.float32))
+    
+    def result(self):
+        # Computes and returns a scalar value for the metric
+        return tf.math.divide_no_nan(self.sum_distance, self.total)
+
+    def reset_state(self):
+        # The state of the metric will be reset at the start of each epoch.
+        self.sum_distance.assign(0)
+        self.total.assign(0)
+
+
+# https://github.com/solivr/tf-crnn/blob/master/tf_crnn/model.py#L157
+# The result of this function is the same as that of the 
+# LevenshteinDistance metric above with normalize = True
+def warp_cer_metric(y_true, y_pred, padding_token=0, use_ctc_decode=False):
+    ''' How to use:
+    from tensorflow.keras.metrics import MeanMetricWrapper
+    cer = MeanMetricWrapper(lambda y_true, y_pred: warp_cer_metric(
+        y_true, y_pred, PADDING_TOKEN, use_ctc_decode=True
+    ), name='cer')
+    '''
+    if use_ctc_decode: y_pred = ctc_decode(y_pred, tf.shape(y_true)[1])
+    y_true = tf.cast(y_true, tf.int64)
+
+    # Get a single batch and convert its labels to sparse tensors.
+    sparse_true = tokens2sparse(y_true, padding_token)
+    sparse_pred = tokens2sparse(y_pred, padding_token)
+
+    # Explain tf.edit_distance: https://stackoverflow.com/questions/51612489
+    edit_distances = tf.edit_distance(sparse_pred, sparse_true, normalize=False)
+
+    # Compute edit distance and total chars count
+    sum_distance = tf.reduce_sum(edit_distances)
+    count_chars = tf.reduce_sum(tf.cast(y_true != padding_token, tf.float32))
+    return tf.math.divide_no_nan(sum_distance, count_chars, name='cer')
