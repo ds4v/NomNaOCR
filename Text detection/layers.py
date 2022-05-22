be9fb5545c326c6dc2ce2675e6a847cf8a91f2fe
Quan Dang <18520339@gm.uit.edu.vn>
2022-05-22 10:55:58 +0700

    Create layers.py and simplify DBNet implementation

---
+++ Text detection/layers.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Convolution2D, Conv2DTranspose, BatchNormalization, Activation
+
+
+class ConvBnRelu(tf.keras.layers.Layer):
+    def __init__(self, filters=64, kernel_size=3, use_bias=True, name='ConvBnRelu', **kwargs):
+        super(ConvBnRelu, self).__init__(name=name, **kwargs)
+        self.conv = Convolution2D(
+            filters, kernel_size, padding='same', 
+            kernel_initializer='he_normal', use_bias=use_bias
+        )
+        self.bn = BatchNormalization()
+        self.relu = Activation('relu')
+        
+    def call(self, inputs):
+        x = self.conv(inputs)
+        x = self.bn(x)
+        return self.relu(x)
+
+
+class DeconvolutionalMap(tf.keras.layers.Layer):
+    def __init__(self, filters=64, name='DeconvolutionalMap', **kwargs):
+        super(DeconvolutionalMap, self).__init__(name=name, **kwargs)
+        self.conv_bn = ConvBnRelu(filters, 3, use_bias=False)
+        self.deconv1 = Conv2DTranspose(filters, 2, strides=2, kernel_initializer='he_normal', use_bias=False)
+        self.bn2 = BatchNormalization()
+        self.relu2 = Activation('relu')
+        self.deconv2 = Conv2DTranspose(1, 2, strides=2, kernel_initializer='he_normal', activation='sigmoid')
+        
+    def call(self, inputs):
+        x = self.conv_bn(inputs)
+        x = self.deconv1(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        return self.deconv2(x)
