ac8aa0423ef378a260393156675c227a169d66b0
Quan Dang <18520339@gm.uit.edu.vn>
2023-04-13 07:29:53 +0700

    Initialize Evaluation folder for Text detection

---
+++ Text detection/Evaluation/zip_val_imgs.sh
@@ -0,0 +1,107 @@
+%cd "/tmp/validate_all/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate_poem/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate_prose/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-1 Quyen thu/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-2 Ngoai ky toan thu/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-3 Ban ky toan thu/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-4 Ban ky thuc luc/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-5 Ban ky tuc bien/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate/Luc Van Tien/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate/Tale of Kieu 1866/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate/Tale of Kieu 1871/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate/Tale of Kieu 1872/truth"
+!zip -q -r 'truth.zip' '.'
+
+%cd "/tmp/validate_all/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate_poem/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate_prose/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-1 Quyen thu/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-2 Ngoai ky toan thu/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-3 Ban ky toan thu/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-4 Ban ky thuc luc/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-5 Ban ky tuc bien/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate/Luc Van Tien/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate/Tale of Kieu 1866/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate/Tale of Kieu 1871/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate/Tale of Kieu 1872/db"
+!zip -q -r 'db.zip' '.'
+
+%cd "/tmp/validate_all/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate_poem/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate_prose/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-1 Quyen thu/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-2 Ngoai ky toan thu/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-3 Ban ky toan thu/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-4 Ban ky thuc luc/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate/DVSKTT-5 Ban ky tuc bien/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate/Luc Van Tien/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate/Tale of Kieu 1866/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate/Tale of Kieu 1871/east"
+!zip -q -r 'east.zip' '.'
+
+%cd "/tmp/validate/Tale of Kieu 1872/east"
+!zip -q -r 'east.zip' '.'
